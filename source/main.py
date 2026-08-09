74c9493d9bdc6d48fa78b6a6019b1a16571f569c
Jethro Naungayan <jethronaungayan0000@gmail.com>
2026-08-09 22:20:41 +0800

    Commit 2: Added additional comments to main.py to better describe the code.

--- source/main.py
+++ source/main.py
@@ -5,16 +5,16 @@ def check_odd_even(number):
     else:
         return "EVEN"
 
-if __name__ == "__main__":
+if __name__ == "__main__":      # Ensures the code below only runs when the script is executed directly
     print("-------------- ODD & EVEN NUMBER CHECKER --------------")
     
     try:
         user_input = input("Enter a whole number: ")
-        # This will trigger a ValueError if a letter or decimal is entered
-        valid_number = int(user_input) 
+        valid_number = int(user_input)       # This will trigger a ValueError if a letter or decimal is entered
     except ValueError:
         print("The value is incorrect. Letters and fractions are not allowed.")
         exit()
     
-    result = check_odd_even(valid_number)
+    result = check_odd_even(valid_number)    # Calls the function to check if the number is odd or even
+
     print(f"Result: {valid_number} is an {result} number.")
